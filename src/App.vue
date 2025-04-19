78290e536ad5c02dda845c7b3e8e70048e6be18e
Cyufan1867 <2669631734@qq.com>
2025-04-19 21:01:44 +0800

    注册登录 静态结构 & 基本切换

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,8 @@
-<script setup>
-import { useUserStore, useCounterStore } from '@/stores/'
-
-const userStore = useUserStore()
-const counterStore = useCounterStore()
-</script>
+<script setup></script>
 
 <template>
   <div>
-    <hr />
     <router-view></router-view>
-    <hr />
-    <hr />
-    <hr />
-    <el-button @click="$router.push('/home')">跳首页</el-button>
-    <el-button type="primary">Primary</el-button>
-    <el-button type="success">Success</el-button>
-    <p>{{ userStore.token }}</p>
-    <el-button @click="userStore.setToken('Event ahsisisisi')">登录</el-button>
-    <el-button @click="userStore.removeToken()">退出</el-button>
-    <hr />
-    {{ counterStore.count }}
-    <el-button @click="counterStore.increment()">Danger</el-button>
   </div>
 </template>
 
