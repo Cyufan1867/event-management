28a312ff009869be0e18beb3ba7075ae94591d3e
Cyufan1867 <2669631734@qq.com>
2025-04-24 15:38:41 +0800

    个人中心-更新个人信息

--- src/api/user.js
+++ src/api/user.js
@@ -7,3 +7,6 @@ export const userLoginService = ({ username, password }) =>
   request.post('api/login', { username, password })
 // 获取用户信息服务
 export const userGetInfoService = () => request.get('/my/userinfo')
+// 更新用户信息服务
+export const userUpdateInfoService = ({ id, nickname, email }) =>
+  request.put('/my/userinfo', { id, nickname, email })
