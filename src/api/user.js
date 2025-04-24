897e4f4902ee564144494c5b30e039caec99012a
Cyufan1867 <2669631734@qq.com>
2025-04-24 15:41:52 +0800

    个人中心-重置密码

--- src/api/user.js
+++ src/api/user.js
@@ -10,3 +10,6 @@ export const userGetInfoService = () => request.get('/my/userinfo')
 // 更新用户信息服务
 export const userUpdateInfoService = ({ id, nickname, email }) =>
   request.put('/my/userinfo', { id, nickname, email })
+// 更新用户头像服务
+export const userUpdateAvatarService = (avatar) =>
+  request.patch('/my/update/avatar', { avatar })
